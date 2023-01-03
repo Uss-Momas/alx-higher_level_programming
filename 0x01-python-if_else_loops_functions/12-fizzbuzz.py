b8cc8b88211129add07da95a029c05be70d492af
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-03 21:10:26 +0200

    Fixing pycodestyle and check requirement

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def fizzbuzz():
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
-            print("FizzBuzz ",end="")
+            print("FizzBuzz ", end="")
         elif number % 3 == 0:
             print("Fizz ", end="")
         elif number % 5 == 0:
