5d26286e79d06ae20b6201f76d71aa8274dfe730
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-05 19:32:32 +0200

    Fixing pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,6 +2,7 @@
 from sys import argv, exit
 import calculator_1 as calc
 
+
 def main():
     args = len(argv) - 1
     if (args != 3):
@@ -12,16 +13,17 @@ def main():
         b = int(argv[3])
         op = argv[2]
         if op == '+':
-            print ("{} {} {} = {}".format(a, op, b, calc.add(a, b)))
+            print("{} {} {} = {}".format(a, op, b, calc.add(a, b)))
         elif op == '-':
-            print ("{} {} {} = {}".format(a, op, b, calc.sub(a, b)))
+            print("{} {} {} = {}".format(a, op, b, calc.sub(a, b)))
         elif op == '*':
-            print ("{} {} {} = {}".format(a, op, b, calc.mul(a, b)))
+            print("{} {} {} = {}".format(a, op, b, calc.mul(a, b)))
         elif op == '/':
-            print ("{} {} {} = {}".format(a, op, b, calc.div(a, b)))
+            print("{} {} {} = {}".format(a, op, b, calc.div(a, b)))
         else:
-                print("Unknown operator. Available operators: +, -, * and /")
-                exit(1)
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+
 
 if __name__ == '__main__':
     main()
