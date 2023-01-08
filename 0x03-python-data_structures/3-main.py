04c55b0462d1535d0dbe3891863808250ff02179
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-08 14:14:48 +0200

    Code refactoration

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -3,3 +3,7 @@ print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_
 
 my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
+lst = [10, 8, 6, 4, 2, 0]
+print_reversed_list_integer(lst)
+ls = []
+print_reversed_list_integer(ls)
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,7 +5,5 @@ def new_in_list(my_list, idx, element):
         return cp_list
     if idx >= len(my_list):
         return cp_list
-    for i in cp_list:
-        if i == idx:
-            cp_list[i] = element
+    cp_list[i] = element
     return cp_list
