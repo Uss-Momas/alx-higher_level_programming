02930f331750580c31c3284290ca106c764035c5
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-08 16:46:35 +0200

    Code refactoring

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    max_value = 0
+    max_value = my_list[0]
     for i in range(0, len(my_list)):
-        if my_list[i] >= max_value:
+        if my_list[i] > max_value:
             max_value = my_list[i]
     return max_value
