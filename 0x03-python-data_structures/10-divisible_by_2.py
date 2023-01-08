536ef3a06a148b4259d3e254f2c054c18a541049
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-08 15:31:16 +0200

    Fixing pycodestyle"

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list= []
+    new_list = []
     for element in my_list:
         if element % 2 == 0:
             new_list.append(True)
