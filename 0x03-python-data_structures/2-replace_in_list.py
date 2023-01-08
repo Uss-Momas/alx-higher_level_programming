dc3b110c3bf0d03bc825c3c58a6e614ac81c6ca8
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-08 13:57:35 +0200

    Fixing code

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    if idx >= len(my_list):
+    if idx > len(my_list):
         return my_list
     for i in my_list:
         if i == idx:
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    result = ()
+    for i in range(len(tuple_a)):
+        result = a, 
+    return result
