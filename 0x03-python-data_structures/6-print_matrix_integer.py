cd79028f062957b2f2870cf3f0d811d6e5022916
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-08 14:30:20 +0200

    Fixing code printing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}".format(j), end=' ')
-        print("")
+            print("{:d}".format(matrix[i][j]), end=' ')
+        print()
