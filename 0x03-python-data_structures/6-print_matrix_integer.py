f5b66c3332cd06bfd6612836acf358ad738497b6
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-08 14:26:54 +0200

    Code refactoring

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             print("{:d}".format(j), end=' ')
-        print("".format())
+        print("")
