eb6eadb57dc1863acc14529884ca29a0132793a7
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-08 15:14:35 +0200

    Func to print sum of two tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     result = ()
-    for i in range(len(tuple_a)):
-        result = a, 
-    return result
+   # if (len(tuple_a) and len(tuple_b)) == 2:
+   #     return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    tuple_c = tuple(zip(tuple_a, tuple_b))
+    result = sum(tuple_c[0]), sum(tuple_c[1])
+    return tuple_c
