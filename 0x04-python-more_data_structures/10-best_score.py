c29c2ed7072dbae96c929a061967447e48fd60f1
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-11 22:35:34 +0200

    Fixing pycodestyle

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     max_value = max(a_dictionary.values())
     for value in a_dictionary.values():
