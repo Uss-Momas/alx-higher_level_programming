7edaf5272660345d8ddf77ef9a44109758332e89
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-11 22:36:44 +0200

    Removing unecessary lines

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,4 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     max_value = max(a_dictionary.values())
-    for value in a_dictionary.values():
-        print(value, end=" ")
     return max_value
