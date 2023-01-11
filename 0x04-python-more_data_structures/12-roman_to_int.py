cfdb928bb94f0a17571ba187bda94b58c7f67f9c
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-11 23:41:48 +0200

    First Version of Roman to Integer conversor

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return None
+    letters = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    ro_str = roman_string
+    value = 0
+    for i in range(0, len(ro_str)):
+        if (i + 1) != len(ro_str):
+            prev, next_v = letters[ro_str[i]], letters[ro_str[i + 1]]
+            if prev >= next_v:
+                value += prev
+            else:
+                value -= prev
+        else:
+            value += letters[ro_str[i]]
+    return value
