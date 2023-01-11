1cda606d56dcf7007ae02ee570b03e65a06a8b16
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-12 00:13:48 +0200

    Reformulating the conditions using only the map function

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x ** 2, matrix_seq)) for matrix_seq in matrix]
+    return list(map(lambda seq: list(map(lambda x: x ** 2, seq)), matrix))
