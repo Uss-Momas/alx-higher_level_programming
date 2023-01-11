8ee2f659c858d8016fd49ac53c37e218da996210
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-11 22:34:17 +0200

    Finding the max value of a dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    max_value = max(a_dictionary.values())
+    for value in a_dictionary.values():
+        print(value, end=" ")
+    return max_value
