296510df0d60f887412f58d6bbdb08646fbedb8d
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-11 22:44:17 +0200

    Now returning the key that points to the max value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     max_value = max(a_dictionary.values())
-    return max_value
+    for key in a_dictionary:
+        if a_dictionary[key] == max_value:
+            return key
+    return None
