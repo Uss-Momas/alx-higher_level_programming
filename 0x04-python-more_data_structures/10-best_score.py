33c98b3d673e53980bfe460546d3123267f060d6
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-11 22:46:22 +0200

    Verifying if it's a dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not isinstance(a_dictionary, dict) or a_dictionary is None:
         return None
     max_value = max(a_dictionary.values())
     for key in a_dictionary:
