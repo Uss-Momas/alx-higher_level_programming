644c83db3a9b994cd1ac69d19dc7e31d67caa7ba
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-12 19:21:47 +0200

    Fixing the problem of not being a string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) != 'str':
+    if roman_string is None or type(roman_string) != str:
         return 0
     letters = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     ro_str = roman_string
