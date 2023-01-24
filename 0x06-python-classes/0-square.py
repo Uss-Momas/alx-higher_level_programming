a5eaa96dcfc51c20d58a41f0d058c1e0c2a06606
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-24 15:35:42 +0200

    Add square class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+class Square:
+    """
+    This class is the responsable to create an object of a square
+
+    example:
+        **
+
+    Attributes:
+        **
+
+    Todo:
+        nothing
+    """
+    pass
