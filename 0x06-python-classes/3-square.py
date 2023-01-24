761e1604714a2713bd76ddb2b3372f97203d3f69
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-24 16:46:30 +0200

    Square class containing the function to calculate the area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """ Declares a square class """
+
+    def __init__(self, size=0):
+        """init function documentation
+
+        Args:
+            size (int): the size of the square
+
+        Returns:
+            None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Calculates the area of the square
+        args:
+            no arguments
+        Return:
+            current area
+        """
+        return self.__size ** 2
