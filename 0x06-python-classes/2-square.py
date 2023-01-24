2d14458961ff2cff8553b7224c830a289138042c
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-24 16:38:22 +0200

    Create protection to the size att

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,9 +3,20 @@
 
 
 class Square:
-    """ Declares a square class
-    attributes:
-        size """
+    """ Declares a square class """
 
-    def __init__(self, size):
-        self.__size = size
+    def __init__(self, size=0):
+        """init function documentation
+
+        Args:
+            size (int): the size of the square
+
+        Returns:
+            None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
