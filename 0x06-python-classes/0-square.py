4999a309fe65bd8cc3ad011659d436c6457b2192
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-24 15:50:11 +0200

    0-square.py docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python
 
-"""
-    0-square module  containing the class module
-    and it's attributes and methods
-    and also other functions
+"""0-square module  containing the class module
+and it's attributes and methods
+and also other functions
 """
 
 
 class Square:
-    """
-    This class is the responsable to create an object of a square
-
-    example:
-        **
-
-    Attributes:
-        **
-
-    Todo:
-        nothing
+    """This class is the responsable to create an object of a square
     """
 
     pass
