70077c2036f74f1c4b8fdff8fcc768d7a3271f12
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-26 21:20:04 +0200

    Add more docstring comments

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""
+""" 0-add_integer module
 Module containing the function that adds integers
 """
 
@@ -7,9 +7,11 @@ Module containing the function that adds integers
 def add_integer(a, b=98):
     """
     Function that add's two integers
+
     Args:
         a: first number
         b: second number, default is 98
-    Return:
+
+    Returns:
         the addition of a,b : a + b
     """
