dcda072baa9c8994477abbbf126ebcf7436e61dd
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-26 21:37:40 +0200

    Modifying the function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,3 +15,10 @@ def add_integer(a, b=98):
     Returns:
         the addition of a,b : a + b
     """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+
+    a, b = int(a), int(b)
+    return a + b
