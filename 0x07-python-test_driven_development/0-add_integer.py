35e0e4a3dea9dcf945d3151aa274ce272ae3e613
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-26 20:55:55 +0200

    Add the add_integer file and the docstring for the module and file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module containing the function that adds integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Function that add's two integers
+    Args:
+        a: first number
+        b: second number, default is 98
+    Return:
+        the addition of a,b : a + b
+    """
