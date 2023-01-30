2c1bde58a0490d771142e0ecd88c7786866464e6
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-30 13:38:26 +0200

    Creating the rectangle file

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""The 0-rectangle Module"""
+
+
+class Rectangle:
+    """Class Rectangle containg methods and attributes"""
+
+    def __init__(self):
+        """ Init method
+        initial method created when a new object is created
+        """
+        pass
