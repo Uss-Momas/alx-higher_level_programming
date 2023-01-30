485fc9717cb869eb4420b765e4dafdd624187e59
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-01-30 13:49:05 +0200

    Creating the rectangle property based on the 0-rectangle template

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""The 0-rectangle Module"""
+
+
+class Rectangle:
+    """Class Rectangle containg methods and attributes"""
+
+    def __init__(self, width=0, height=0):
+        """ Init method
+        initial method created when a new object is created
+
+        Args:
+            width: the width of the rectangle, 0 is default
+            height: the height of the rectangle, 0 is default
+
+        Returns:
+            Nothing
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Getter method for private instance width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for the private att width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method for the height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for the height attribute"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
