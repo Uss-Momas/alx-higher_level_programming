71528b0b6fbb8c5335171676ed22b0af8a7d9b7e
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-02-07 22:37:45 +0200

    REloading from json file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Student class Module
+"""
+
+
+class Student:
+    """ Student class representing
+    the Student information
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of a Student instance
+        """
+        my_dict = {}
+        is_list_strings = True
+        if attrs is not None:
+            for att in attrs:
+                if type(att) is not str:
+                    is_list_strings = False
+                    break
+            if is_list_strings:
+                for key in attrs:
+                    if self.__dict__.get(key):
+                        my_dict[key] = self.__dict__[key]
+            return my_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Reloads information from a json
+        """
+        self.__dict__ = json
