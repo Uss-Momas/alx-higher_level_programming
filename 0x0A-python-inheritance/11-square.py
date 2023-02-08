c0591aa7046aa8abb5a83990f5d048cd7b6ce688
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-02-09 00:43:23 +0200

    Square 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Square Module
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ Square class that inheritate the Rectangle class
+    """
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
