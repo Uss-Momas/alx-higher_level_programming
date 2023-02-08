e9642702d39ea027d71fb40690c8d475c5afc5f4
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-02-08 23:46:27 +0200

    Validating value

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Base geometry Module
+"""
+
+
+class BaseGeometry(object):
+    """Base Geometry class
+    """
+    def area(self):
+        """ function that computes the area of a base geometry object
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validades the value attribute
+        Args:
+            name: a string
+            value: the value to be validated
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0")
+
+        self.value = value
