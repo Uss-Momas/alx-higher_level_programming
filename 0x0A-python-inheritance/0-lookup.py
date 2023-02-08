c56d5b278bd0d4c3f3794ed964287b0bf3bc727b
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-02-08 22:24:10 +0200

    Lookup for attributes and fu adn methods of a given object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Lookup Module
+"""
+
+
+def lookup(obj):
+    """Function that returns list of availables attributes
+    and Methods of an object
+    """
+    return dir(obj)
