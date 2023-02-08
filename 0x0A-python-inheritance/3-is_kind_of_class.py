6e6735ae790c1a7e79570f129dd5d9a7868cd0ad
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-02-08 23:18:47 +0200

    using isinstance to class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""is kind class Module
+"""
+
+
+def is_kind_class(obj, a_class):
+    """ Verifyes if a obj is a instance of the class
+    """
+    return isinstance(obj, a_class)
