79cb89d19cb5d8bfc8ee17928c8da4b6e7e45095
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-02-08 23:19:24 +0200

    changing method name

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""is kind class Module
+"""is kind of class Module
 """
 
 
-def is_kind_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """ Verifyes if a obj is a instance of the class
     """
     return isinstance(obj, a_class)
