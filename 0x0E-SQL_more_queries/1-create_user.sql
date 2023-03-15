e443caa33c6f6c872642b0bccf4d385324c6f056
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-03-15 22:23:36 +0200

    Flushing privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- creating a user
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED WITH auth_socket BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
