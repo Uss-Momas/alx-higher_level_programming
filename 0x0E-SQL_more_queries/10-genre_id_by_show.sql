e3fced1cc568606e8ca32034186cc360ca1b8b6b
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-03-18 16:40:12 +0200

    Left Joiner

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,8 @@
 -- Making join of 3 tables
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id;
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.genre_id IS NOT NULL;
