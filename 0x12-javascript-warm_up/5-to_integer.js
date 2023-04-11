05b988ccb0b0e859aeb0520ef1a67c8b8ee3bba3
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-11 20:46:08 +0200

    Verying the requirement checks

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,11 @@
 #!/usr/bin/node
 
 const process = require('process');
+const argv = process.argv;
+const integer = Number(argv[2]);
+
+if (!isNaN(integer)) {
+  console.log('My number: ');
+} else {
+  console.log('Not a number');
+}
