fe336ae685fbb4af09435b7e7b97cf1d4b3e2f8c
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-11 21:31:15 +0200

    Fixing semistandard linter errors

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,8 +4,9 @@ const argv = process.argv;
 const number = parseInt(Number(argv[2]));
 
 function factorial (n) {
-  if (n === 0)
+  if (n === 0) {
     return (1);
+  }
   return (n * factorial(n - 1));
 }
 
