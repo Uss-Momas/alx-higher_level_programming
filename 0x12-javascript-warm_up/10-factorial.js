3f32786a6f4dd1cecad3c6b20b3bdcbfc02c9804
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-11 21:29:25 +0200

    calculating factorial of a number

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+const number = parseInt(Number(argv[2]));
+
+function factorial (n) {
+  if (n === 0)
+    return (1);
+  return (n * factorial(n - 1));
+}
+
+if (isNaN(number)) {
+  console.log(factorial(1));
+} else {
+  console.log(factorial(number));
+}
