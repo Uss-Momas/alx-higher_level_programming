7245e5efb2775509f2a244694b3b2596eb30b952
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-11 20:47:22 +0200

    Parsing the integer provided in the argument list

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@ const argv = process.argv;
 const integer = Number(argv[2]);
 
 if (!isNaN(integer)) {
-  console.log('My number: ');
+  console.log('My number: ' + parseInt(integer));
 } else {
   console.log('Not a number');
 }
