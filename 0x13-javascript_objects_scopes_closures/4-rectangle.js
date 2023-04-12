d578a0fa54136c43ba6cfc75050004e949d2ef63
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-12 20:14:54 +0200

    adding rotate and double features, rotate exchange w and h, while double() doubles the values of w and h

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (!(isNaN(Number(w)) || isNaN(Number(h)))) {
+      if (Number(w) > 0 && Number(h) > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  }
+
+  print () {
+    const lineX = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(lineX);
+    }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
