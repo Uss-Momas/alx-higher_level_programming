ff3d36eca5a08b76eba394aad1d056c11731d1d6
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-12 20:22:17 +0200

    refactoring the code

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,11 +2,9 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (!(isNaN(Number(w)) || isNaN(Number(h)))) {
-      if (Number(w) > 0 && Number(h) > 0) {
-        this.width = w;
-        this.height = h;
-      }
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
 
