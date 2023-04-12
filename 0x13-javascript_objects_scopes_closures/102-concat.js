0eeb36a79110a8b0a92e7f542deba427682556e6
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-12 22:20:57 +0200

    fixing linter errors

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,8 +3,8 @@
 const argv = process.argv;
 const fs = require('fs');
 
-const content1 = fs.readFileSync(argv[2], {encoding: 'utf-8', flag: 'r'});
-const content2 = fs.readFileSync(argv[3], {encoding: 'utf-8', flag: 'r'});
+const content1 = fs.readFileSync(argv[2], { encoding: 'utf-8', flag: 'r' });
+const content2 = fs.readFileSync(argv[3], { encoding: 'utf-8', flag: 'r' });
 
 const content3 = content1 + content2;
 
