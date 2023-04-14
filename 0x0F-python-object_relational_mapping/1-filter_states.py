45a465c0724bdbd73bd6796db808ab322715fae1
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-14 19:10:39 +0200

    fixing if condition

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""Select States Module"""
+"""
+filter states Module
+Lists all states that starts name with 'N'
+"""
 
 import MySQLdb
 from sys import argv
 
-hst = 'localhost'
-prt = 3306
-user = argv[1]
-passw = argv[2]
-db = argv[3]
+if __name__ == "__main__":
+    hst = 'localhost'
+    prt = 3306
+    user = argv[1]
+    passw = argv[2]
+    db = argv[3]
 
-con = MySQLdb.connect(host=hst, port=prt, user=user, passwd=passw, db=db)
-cur = con.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-con.close()
+    con = MySQLdb.connect(host=hst, port=prt, user=user, passwd=passw, db=db)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    con.close()
