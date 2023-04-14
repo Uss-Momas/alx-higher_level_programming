6e630b8f1c329b88a0e380692067a9e021b913a3
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-14 20:55:23 +0200

    fixing the query

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
             """SELECT cities.name FROM cities
             JOIN states
             ON cities.state_id = states.id
-            WHERE states.name=%s
+            WHERE states.name LIKE %s
             ORDER BY cities.id ASC
             """, (state_name,)
             )
