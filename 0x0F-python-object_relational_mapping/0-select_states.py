816d9342efde7eceaa707ae395eed6829604d3d6
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-14 19:04:33 +0200

    Fixing pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,13 +9,11 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "main":
-    hst = 'localhost'
-    prt = 3306
-    user = argv[1]
-    passw = argv[2]
-    db = argv[3]
+    con = MySQLdb.connect(
+            host="localhost", port=3306, user=argv[1], passwd=argv[2],
+            db=argv[3]
+            )
 
-    con = MySQLdb.connect(host=hst, port=prt, user=user, passwd=passw, db=db)
     cur = con.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
