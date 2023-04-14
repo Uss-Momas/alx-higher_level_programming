23c195f818809856d2fe414b43b00ee84deecf43
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-14 19:07:52 +0200

    fixing the if verifying th name if main

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ to lists all states from a database
 import MySQLdb
 from sys import argv
 
-if __name__ == "main":
+if __name__ == "__main__":
     con = MySQLdb.connect(
             host="localhost", port=3306, user=argv[1], passwd=argv[2],
             db=argv[3]
