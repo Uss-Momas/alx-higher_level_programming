c0869ce9f1009d7ac540cc895ee08aa9fc1bfaa1
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-14 18:59:41 +0200

    Fixing module documentation and controlling if the main is being executed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""Select States Module"""
+"""
+Select States Module
+Module that uses MySQLdb in simple form
+to lists all states from a database
+"""
 
 import MySQLdb
 from sys import argv
 
-hst = 'localhost'
-prt = 3306
-user = argv[1]
-passw = argv[2]
-db = argv[3]
+if __name__ == "main":
+    hst = 'localhost'
+    prt = 3306
+    user = argv[1]
+    passw = argv[2]
+    db = argv[3]
 
-con = MySQLdb.connect(host=hst, port=prt, user=user, passwd=passw, db=db)
-cur = con.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-con.close()
+    con = MySQLdb.connect(host=hst, port=prt, user=user, passwd=passw, db=db)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    con.close()
