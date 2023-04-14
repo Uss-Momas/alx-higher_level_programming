8128580656aa7a2c4d9f33cbdf52dfc8c8e3fc26
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-14 19:58:50 +0200

    Filtering in safe way

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+3-my_safe_filter_states Module
+Filtering the states table in a safe way
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    con = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+
+    state_name_searched = argv[4]
+    print(state_name_searched)
+    cur = con.cursor()
+    cur.execute(
+            "SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+            (state_name_searched,)
+            )
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    con.close()
