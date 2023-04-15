638ee75855aa1a8165b01865bfae15a7b74956a1
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-15 22:09:10 +0200

    Refactoring th etg the table

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -13,21 +13,15 @@ if __name__ == '__main__':
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(argv[1], argv[2], argv[3])
                            )
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     results = session.query(State, City).join(State).order_by(
             State.id.asc(), City.id.asc()
             )
-    table = results.all()
-    states = []
-    for state, city in table:
-        if state not in states:
-            states.append(state)
-
+    states = session.query(State).all()
     for state in states:
         print('{}: {}'.format(state.id, state.name))
-        for st, city in table:
-            if city.state_id == state.id:
-                print('\t{}: {}'.format(city.id, city.name))
-
+        for city in state.cities:
+            print('\t{}: {}'.format(city.id, city.name))
     session.close()
