4acc7ad2d50ac52c5c93f0da97c5d4fb7ddfb295
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-15 15:00:13 +0200

    using sessionamker to get the first object of State in the database

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,13 +5,18 @@
 from model_state import Base, State
 from sys import argv
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine(
             "mysql+mysqldb://{}:{}@localhost:3306/{}"
             .format(argv[1], argv[2], argv[3])
             )
-    results = engine.execute("SELECT * FROM states")
-    row = results.fetchone()
-    if len(row) > 0:
-        print('{}: {}'.format(row[0], row[1]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # will return a State object
+    result = session.query(State).first()
+    if result is None:
+        print('Nothing')
+    else:
+        print("{}: {}".format(result.id, result.name))
