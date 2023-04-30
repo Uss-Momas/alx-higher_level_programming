8017c8303a0e9cf10462f9e31f7b8dc43b8015da
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-30 16:12:23 +0200

    changing the url route

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -s 0.0.0.0:5000/catch_me -L -X PUT -H "Origin: HolbertonSchool"
+curl -s 0.0.0.0:5000/catch_me_3 -L -X PUT -H "Origin: HolbertonSchool"
