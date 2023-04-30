a1d8094fbdf110454dd19641a1b540a1d7702391
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-30 16:01:45 +0200

    removing '\n' from the string

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sI -o /dev/null -w "%{response_code}\n" "$1"
+curl -sI -o /dev/null -w "%{response_code}" "$1"
