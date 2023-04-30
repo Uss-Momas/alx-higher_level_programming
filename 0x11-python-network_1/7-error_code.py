3c06f82668ff9007128d7c950d4ce2301e5989e4
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-30 18:42:37 +0200

    Printing status code for a ulr

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL,
+sends a request to the URL and displays the body of the response.
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    res = requests.get(argv[1])
+    code = res.status_code
+    if code >= 400:
+        print('Error code: {}'.format(code))
+    else:
+        print(res.text)
