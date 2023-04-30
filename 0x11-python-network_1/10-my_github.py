229a546b52a75ce3c7bf4f4da8898d3c8641dca2
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-30 19:38:16 +0200

    Using Basic Authentication to get user id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,4 +3,22 @@
 Python script that takes your GitHub credentials (username and password)
 and uses the GitHub API to display your id
 """
+import requests
+from sys import argv
 
+
+if __name__ == "__main__":
+    github_api_url = 'https://api.github.com'
+    username = argv[1]
+    password = argv[2]
+    headers = {'Authorization': 'token {}'.format(password),
+               'X-GitHub-Api-Version': '2022-11-28',
+               'Accept': 'application/vnd.github+json'
+               }
+
+    res = requests.get('{}/user'.format(github_api_url), headers=headers)
+    result = res.json().get('id')
+    if result:
+        print(result)
+    else:
+        print(None)
