4f760147c10bca499f6443001a051b5997b3eeb8
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-04-30 16:47:40 +0200

    Add condition if it's the main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,11 @@ Python scrip that fetches https://alx-intranet.hbtn.io/status
 """
 from urllib import request
 
-req = request.Request('https://alx-intranet.hbtn.io/status')
-with request.urlopen(req) as response:
-    html = response.read()
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-          .format(type(html), html, str(html, 'utf-8')))
+if __name__ == "__main__":
+    req = request.Request('https://alx-intranet.hbtn.io/status')
+    with request.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(str(html, 'utf-8')))
