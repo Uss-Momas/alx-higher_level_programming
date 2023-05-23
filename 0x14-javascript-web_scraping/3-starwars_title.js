cc349fa2ad50ac35056fc87999ab0e0c19e1bf97
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-05-23 21:56:39 +0200

    Fixing semistandard error

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,8 +2,8 @@
 
 const request = require('request');
 const process = require('process');
-const id = process.argv[2]
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request(url, (error, response, body) => {
   if (error) console.log(error);
