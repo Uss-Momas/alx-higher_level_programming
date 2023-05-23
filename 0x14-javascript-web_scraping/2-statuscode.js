144a36924c36b55ff0558c12d1e23ba4242a93f9
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-05-23 21:40:29 +0200

    Making simple request with request module

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+const process = require('process');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+  console.log("Code: " + response.statusCode);
+});
