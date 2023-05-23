80533228b8afa172e53f10fbdba4e36dd2e4308a
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-05-23 21:41:49 +0200

    Fixing semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,5 +6,5 @@ const url = process.argv[2];
 
 request(url, (error, response, body) => {
   if (error) console.log(error);
-  console.log("Code: " + response.statusCode);
+  console.log('Code: ' + response.statusCode);
 });
