7a1482d6753004960eb0476329ae2bc7d18ea95f
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-05-23 23:00:24 +0200

    Using JS object to save id's and number of tasks completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const process = require('process');
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) console.log(err);
+  const json = JSON.parse(body);
+  let count = 0;
+  let resp = {};
+  for (let i = 1; i <= 10; i++) {
+    for (let j = 0; j < json.length; j++) {
+      if (json[j].userId === i) {
+        if (json[j].completed === true) {
+          count++;
+        }
+      }
+    }
+    resp[i] = count;
+    count = 0;
+  }
+  console.log(resp)
+});
