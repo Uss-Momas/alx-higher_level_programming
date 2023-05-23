27443adee94cecc8afb970683006b8322385c2e1
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-05-23 21:55:45 +0200

    Getting body of response and parsing as JSON

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const process = require('process');
+const id = process.argv[2]
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`
+
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+  const json = JSON.parse(body);
+  console.log(json.title);
+});
