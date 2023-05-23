96d8e4f255581fdc7f4e9f968b26f5abf79b7e61
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-05-23 22:26:19 +0200

    filtering by the url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,19 @@
 #!/usr/bin/node
 
 const request = require('request');
-// const process = require('process');
-// const url = process.argv[2];
-const personUrl = 'https://swapi-api.alx-tools.com/api/people/18';
+const process = require('process');
+const url = process.argv[2];
+const personUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 
-request(personUrl, (err, response, body) => {
+let count = 0;
+request(url, (err, response, body) => {
   if (err) console.log(err);
-  const json = JSON.parse(body);
-  console.log(json.films.length);
+  const json = JSON.parse(body).results;
+  for (let i = 0; i < json.length; i++) {
+    if (json[i].characters.includes(personUrl)) {
+      count++;
+    }
+  }
+  console.log(count);
 });
+
