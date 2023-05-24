5419cde1b686e543b44090ffdb3fb2d52d736d99
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-05-24 20:17:58 +0200

    Fixing semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,16 +3,15 @@
 const request = require('request');
 const process = require('process');
 const url = process.argv[2];
-const personUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 let count = 0;
 request(url, (err, response, body) => {
   if (err) console.log(err);
   const json = JSON.parse(body).results;
   for (let i = 0; i < json.length; i++) {
-    let characters = json[i].characters;
+    const characters = json[i].characters;
     for (let j = 0; j < characters.length; j++) {
-      if (characters[j].search('18') != -1) {
+      if (characters[j].search('18') !== -1) {
         count++;
       }
     }
