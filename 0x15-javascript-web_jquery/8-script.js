a0bf37c4e6dbf1f785fd4dec98f2b2380a75aeab
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-06-12 20:19:24 +0200

    Using AJAX to fetch movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+$list_movies = $('UL#list_movies');
+
+$.ajax({
+  type: 'GET',
+  url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+  success: function(data) {
+    $.each(data.results, function (i, film){
+      $list_movies.append(`<li> ${film.title} </li>`);
+    });
+  } 
+});
