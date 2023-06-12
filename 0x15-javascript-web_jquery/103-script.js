35a6548a744c73f9489c621f167453ad56ecfaf4
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-06-12 22:06:10 +0200

    Enter key event controlling

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+
+  $('INPUT#btn_translate').on('click', function () {
+    const $lang = $('INPUT#language_code').val();
+
+    $.ajax({
+      type: 'GET',
+      url: `https://hellosalut.stefanbohacek.dev/?lang=${$lang}`,
+      success: function (data) {
+        $('DIV#hello').text(data.hello);
+      }
+    });
+  });
+
+  $('INPUT#language_code').keypress(function (e){
+    const $key = e.which;
+    if ($key === 13) {
+      const $lang = $('INPUT#language_code').val();
+      $.ajax({
+        type: 'GET',
+        url: `https://hellosalut.stefanbohacek.dev/?lang=${$lang}`,
+        success: function (data) {
+          $('DIV#hello').text(data.hello);
+        }
+      });
+    }
+  });
+
+};
