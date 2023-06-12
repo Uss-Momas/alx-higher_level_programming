9b0679fd3a40e2c00ff4d614a3a2cb7f84907f1b
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-06-12 21:54:18 +0200

    Using an API that translates the Hello in a set of languages

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+
+  $('INPUT#btn_translate').on('click', function () {
+    const $lang = $('INPUT#language_code').val();
+
+    $.ajax({
+      type: 'GET',
+      url: `https://hellosalut.stefanbohacek.dev/?lang=${$lang}`,
+      success: function (data) {
+        $('DIV#hello').text(data.hello);
+      }
+    });
+  });
+
+};
